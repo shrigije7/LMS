b8e2aeb3dcf77aeb0a6877da3bd727c000f01929
shrinath <shrigije7@gmail.com>
2021-05-27 10:34:57 +0530

    resoponsive layout added to book Component

--- src/Book.js
+++ src/Book.js
@@ -1,10 +1,21 @@
 import react from "react"
+import BookDetails from "./BookDetails.js"
+
+
 
 export const Book=(props)=>{
+ 
+    const handleBookClick=()=>
+    {
+        console.log("book Clicked");
+    <BookDetails />
+    
+    
+}
     //console.log(props)
     return(
     
-        <div className="book">
+        <div onClick={ handleBookClick} className="book">
             <img className="book-img" src={props.book.img} />
             <h1>{props.book.title}</h1>
             {/* <p>{p}</p> */}
---
+++ src/BookDetails.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+ const BookDetails=()=>{
+    alert("Book Details called")
+    console.log("Showing book Details");
+    return(
+    <h1>Book Deatails</h1>
+    
+)
+    }
+export default BookDetails
